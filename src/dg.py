30b044eeb70a9c260792833ea6e943192cd0bee2
Alberto-o <albergascon@gmail.com>
2020-05-26 19:08:53 +0200

    1d functions added. Working on jacobi_gauss

--- src/dg.py
+++ src/dg.py
@@ -74,6 +74,56 @@ class Line:
         raise ValueError('N must be positive.')
 
     @staticmethod
+    def jacobi_gauss(alpha, beta, N):
+        """
+        Compute the order N Gauss quadrature points, x, 
+        and weights, w, associated with the Jacobi 
+        polynomial, of type (alpha,beta) > -1 ( <> -0.5).
+        >>> Line.jacobi_gauss(2,1,0)
+        [array([-0.54691816,  0.26120387]), array([0.76094757, 0.57238576])]
+        """
+        x = np.zeros(N)
+        w = np.zeros(N)
+        if N==0:
+            x = -(alpha-beta)/(alpha+beta+2)
+            w = 2
+            return [x,w]
+
+        # Form symmetric matrix from recurrence.
+        J = np.zeros([N+1,N+1])
+        h1 = np.zeros(N+1)
+        aux = np.zeros(N)
+        
+        for i in range(N):
+            aux[i] = 1+i
+
+        for i in range(N+1):
+            h1[i] = 2*i+alpha+beta
+
+        J = np.diag(-0.5*(alpha**2-beta**2)/(h1+2)/h1) \
+            + np.diag(2/(h1[0:N]+2) \
+            * np.sqrt(aux*(aux+alpha+beta)*(aux+alpha) \
+            * (aux+beta)/(h1[0:N]+1)/(h1[0:N]+3)),1)
+
+        eps = np.finfo(np.float).eps
+
+        if (alpha+beta < 10*eps):
+            J[0,0] = 0.0
+
+        J = J+np.transpose(J)
+
+        [V,D] = np.linalg.eig(J); 
+        x = np.diag(D)
+#        vt = np.transpose(V[0,:])
+        w = V**2*2**(alpha+beta+1)/(alpha+beta+1)*scipy.special.gamma(alpha+1) \
+            * scipy.special.gamma(beta+1)/scipy.special.gamma(alpha+beta+1)
+
+        return [x,w]
+
+
+
+
+    @staticmethod
     def jacobi_polynomial(r, alpha, beta, N):
         """
         Evaluate Jacobi Polynomial
@@ -187,7 +237,106 @@ class Line:
 
     @staticmethod
     def differentiation_matrix_1d(N,r,V):
+        """
+        Initialize the (r) differentiation matrices
+        of the interval evaluated at (r) at order N
+        V is the 1d Vandermonde matrix
+        >>> r = Line.jacobi_gauss_lobatto(0,0,2)
+        >>> V = Line.vandermonde_1d(2,r)
+        >>> Line.differentiation_matrix_1d(2,r,V)
+        array([[-1.5,  2. , -0.5],
+               [-0.5,  0. ,  0.5],
+               [ 0.5, -2. ,  1.5]])
 
+        >>> r = Line.jacobi_gauss_lobatto(0,0,3)
+        >>> V = Line.vandermonde_1d(3,r)
+        >>> Line.differentiation_matrix_1d(3,r,V)
+        array([[-3.00000000e+00,  4.04508497e+00, -1.54508497e+00,  5.00000000e-01],
+               [-8.09016994e-01, -4.05396129e-16,  1.11803399e+00, -3.09016994e-01],
+               [ 3.09016994e-01, -1.11803399e+00,  6.28036983e-16,  8.09016994e-01],
+               [-5.00000000e-01,  1.54508497e+00, -4.04508497e+00,  3.00000000e+00]])
+        """
+        Vr = Line.vandermonde_1d_grad(N,r)
+        Vinv = np.linalg.inv(V)
+        Dr = np.matmul(Vr,Vinv)
+        return Dr
+
+    @staticmethod
+    def surface_integral_dg(N,V):
+        """
+        Compute surface integral term in DG formulation
+        >>> r = Line.jacobi_gauss_lobatto(0,0,2)
+        >>> V = Line.vandermonde_1d(2,r)
+        >>> Line.surface_integral_dg(2,V)
+        array([[ 4.5 ,  1.5 ],
+               [-0.75, -0.75],
+               [ 1.5 ,  4.5 ]])
+
+        >>> r = Line.jacobi_gauss_lobatto(0,0,3)
+        >>> V = Line.vandermonde_1d(3,r)
+        >>> Line.surface_integral_dg(3,V)
+        array([[ 8.        , -2.        ],
+               [-0.89442719,  0.89442719],
+               [ 0.89442719, -0.89442719],
+               [-2.        ,  8.        ]])
+        """
+        # Nfaces, Nfp and Np are defined as global variables
+        Nfaces = 1
+        Nfp = 2
+        Np = N+1
+
+        Emat = np.zeros([Np,Nfaces*Nfp])
+        Emat[0,0] = 1.0
+        Emat[Np-1,1] = 1.0
+
+        Vtrans = np.transpose(V)
+        Vi = np.matmul(Vtrans,Emat)
+        Lift = np.matmul(V,Vi)
+        return Lift
+
+    @staticmethod
+    def normals_1d(K):
+        """
+        Compute outward pointing normals at element faces
+        >>> Line.normals_1d(4)
+        array([[-1., -1., -1., -1.],
+               [ 1.,  1.,  1.,  1.]])
+        """
+        # K is the number of elements, derived from the grid info
+        # Nfaces and Nfp are defined as global variables
+        Nfaces = 1
+        Nfp = 2
+        nx = np.zeros([Nfp*Nfaces,K])
+        nx[0,:] = -1.0
+        nx[1,:] = 1.0
+        return nx 
+
+    @staticmethod
+    def filter_1d(N,Nc,s,V):
+        """
+        Initialize 1D filter matrix of size N.
+        Order of exponential filter is (even) s with cutoff at Nc;
+        >>> r = Line.jacobi_gauss_lobatto(0,0,3)
+        >>> V = Line.vandermonde_1d(3,r)
+        >>> Line.filter_1d(3,1,1,V)
+        array([[ 0.3333842 ,  0.9756328 , -0.14240119, -0.1666158 ],
+               [ 0.19512656,  0.66667684,  0.16667684, -0.02848024],
+               [-0.02848024,  0.16667684,  0.66667684,  0.19512656],
+               [-0.1666158 , -0.14240119,  0.9756328 ,  0.3333842 ]])
+        """
+
+        s_even = 2*s
+        Fdiagonal = np.ones(N+1)
+        alpha = -np.log(np.finfo(np.float).eps)
+
+        for i in range(Nc,N+1):
+            Fdiagonal[i] = np.exp(-alpha*((i-Nc)/(N-Nc))**s_even)
+
+        # F = V*diag(Fdiagonal)*Vinv
+        Fi = np.matmul(V,np.diag(Fdiagonal))
+        Vinv = np.linalg.inv(V)
+        F = np.matmul(Fi,Vinv)
+        return F
 
 if __name__ == '__main__':
     import doctest
